2acba3eefb1652fedd9fbca07e0b7d7d1fe21238
alionamalik <115574717+alionamalik@users.noreply.github.com>
2022-10-30 13:12:58 +0300

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,14 +1,12 @@
 def get_count_char(str_):
-    ...  # TODO посчитать количество каждой буквы в строке в аргументе str_
+    # TODO посчитать количество каждой буквы в строке в аргументе str_
     char_dict = {}
-    CHAR_DEFAULT = 0
-    str_.lower()
-    for char in str_:
+    for char in str_.lower():
         if char.isalpha():
-            char_dict[char] = char_dict.get(char, CHAR_DEFAULT) + 1
-    sorted_chart_dict = dict(sorted(char_dict.items()))     #сортирую по алфавиту
+            char_dict[char] = char_dict.get(char, 0) + 1
 
-    return sorted_chart_dict
+    return char_dict
+
 
 main_str = """
     Данное предложение будет разбиваться на отдельные слова. 
@@ -16,22 +14,16 @@ main_str = """
     Далее нужно отсортировать слова в алфавитном порядке, а после сортировки склеить их с помощью метода строк join. Приступим!!!!
 """
 
-main_dict = get_count_char(main_str)     #создаю словарь для второй функции
-def get_percent_char(dict_):
+
+def get_percent_char():
+    main_dict = get_count_char(main_str)
     total_number = sum(main_dict.values())
-    TOTAL = 100
-    new_values = []
-    for value in main_dict.values():        #создаю список с процентами
-        new_values.append(round(value/total_number*TOTAL, 2))
-    i = 0
-    while i < len(new_values):
-        for key, value in main_dict.items():    #объединяю ключи из старого словаря с созданным списком
-            main_dict[key] = new_values[i]
-            i += 1
+    for key, value in main_dict.items():
+        main_dict[key] = round(value/total_number*100, 2)
     print('Проверка:')
     print('Всего процентов:', round(sum(main_dict.values())))
     return main_dict
 
 
-print(get_count_char(main_str)) #первая функция
-print(get_percent_char(main_dict)) #вторая функция
+print(get_count_char(main_str))     #первая функция
+print(get_percent_char())       #вторая функция
