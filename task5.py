2397d004e466112e7d96e0db749517fb58c5f926
alionamalik <115574717+alionamalik@users.noreply.github.com>
2022-11-15 22:18:49 +0300

    Update task5.py

--- task5.py
+++ task5.py
@@ -1,9 +1,9 @@
 import random, string
 
 
-def get_random_password(n=8) -> str:
+def get_random_password(length=8) -> str:
     alphabet = string.ascii_uppercase+string.digits+string.ascii_lowercase
-    password = ''.join(random.sample(alphabet, n))  # TODO написать функцию генерации случайных паролей
+    password = ''.join(random.sample(alphabet, length))  # TODO написать функцию генерации случайных паролей
     return password
 
 
